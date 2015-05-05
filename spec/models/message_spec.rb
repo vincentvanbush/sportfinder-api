7dade65f628b3c9b73dec5cbb48007b88d45656a
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-05-05 17:47:45 +0000

    specs for 'after' scope

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Message do
+  subject { message }
 
+  describe '.after' do
+    before do
+      @now = DateTime.now
+      @event = FactoryGirl.create :event
+      4.times do |i|
+        FactoryGirl.create :message,
+                           event: @event,
+                           created_at: @now + i.minutes,
+                           updated_at: @now + i.minutes
+      end
+    end
+
+    it 'returns the messages created after the specified datetime' do
+      after = @event.messages[2].created_at
+      expected_messages = [@event.messages[2], @event.messages[3]]
+      expect(@event.messages.after(@now + 2.minutes)).to match_array(expected_messages)
+    end
+  end
 end
