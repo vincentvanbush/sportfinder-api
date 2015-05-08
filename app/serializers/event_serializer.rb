d56aae8f9a0d13148a036dbd65938d74a0216171
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-05-08 23:15:36 +0000

    adds additional fields summarizing event's vote counts

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :venue, :start_date, :finished?
+  attributes :id, :title, :description, :venue, :start_date, :finished?,
+             :positive_votes, :negative_votes
 
   # belongs_to :discipline
   has_one :user, serializer: EventUserSerializer
@@ -8,5 +9,13 @@ class EventSerializer < ActiveModel::Serializer
 
   def id
     object.id.to_s
+  end
+
+  def positive_votes
+    object.positive_votes
+  end
+
+  def negative_votes
+    object.negative_votes
   end
 end
