c5d27e988be5c47fd7b207b9446f5aa82f124c0c
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-05-15 00:47:13 +0200

    Populating data in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating categories...'
+titles = [
+	'football',
+	'basketball',
+	'voleyball',
+	# 'F1',
+	'tennis',
+	# 'chess',
+	# 'curling',
+	# 'snooker',
+	# 'golf',
+	# 'dart'
+	'race']
+disciplines = []
+titles.each do |title|
+	disciplines << Discipline.create(title: title)
+end
+
+puts 'Creating users...'
+passwords = ['qweqweqwe', 'asdasdasd', 'zxczxczxc']
+users = []
+20.times do 
+	pass = passwords.sample
+	users << User.create(
+		email: Faker::Internet.email,
+		password: pass,
+		password_confirmation: pass
+	)
+end
+
+puts 'Creating messages...'
+messages = []
+300.times do 
+	messages << Message.new(
+		content: Faker::Lorem.sentences(2)
+	)
+end
+
+puts 'Creating events...'
+events = []
+
+100.times do |i|
+	discipline = disciplines.sample
+	user = users.sample
+ 	contender_params = []
+ 	contender_params << { squad_members: [] }
+	events << Event.create(
+		title: Faker::Lorem.sentence(2, false, 2),
+		description: Faker::Lorem.sentences(3),
+		venue: Faker::Lorem.sentence(2, false, 0),
+		start_date: Faker::Time.forward(14),
+		user: user,
+		discipline: discipline,
+		messages: messages[i*3..i*3+2],
+		contenders: contender_params
+	)
+end
