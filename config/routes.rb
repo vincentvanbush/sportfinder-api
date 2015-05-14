b723cc6c389abdf68663fe25dd32d2e027f12ee7
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-05-15 00:46:51 +0200

    Changes path to destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  # use_doorkeeper
   devise_for :users
   namespace :api, defaults: { format: :json }, path: '/api'  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
+      post '/sessions', to: 'sessions#create'
+      delete '/sessions', to: 'sessions#destroy'
       resources :disciplines, only: [:show, :index] do
         resources :events, only: [:show, :index, :create, :update, :destroy] do
           resources :messages, only: [:index, :create, :update, :destroy]
