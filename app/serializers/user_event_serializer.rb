3196b8fbd30e2e6eacd2744ae77c546ec4a3c4c9
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-05-14 15:03:01 +0000

    user event serializer now serializes discipline_id

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -1,7 +1,7 @@
 class UserEventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :venue, :start_date, :finished?
+  attributes :id, :title, :description, :venue, :start_date, :finished?,
+             :discipline_id
 
-  def id
-    object.id.to_s
-  end
+  def id() object.id.to_s end
+  def discipline_id() object.discipline_id.to_s end
 end
