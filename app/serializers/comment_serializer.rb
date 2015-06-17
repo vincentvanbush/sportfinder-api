5416eac66235d481d88e5782d35a5770b0375585
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-06-17 23:31:22 +0200

    Serializing timestamp in comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :user_email, :content, :created_at
+  attributes :user_email, :content, :created_at, :timestamp
 
   def id() object.id.to_s end
 
   def user_email
     object.user.email
+  end
+
+  def timestamp
+    object.created_at.to_i
   end
 end
